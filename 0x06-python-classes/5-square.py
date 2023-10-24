f3f79266d45961dd9eef510ae17c60eaaa7383ef
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-10-24 16:15:30 -0700

    Task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -33,5 +33,5 @@ class Square:
         """Print in stdout the square."""
         for i in range(self.size):
             for j in range(self.size):
-                print("#", end="\n" if j == self.size - 1and i != j else "")
+                print("#", end="\n" if j == self.size - 1 and i != j else "")
         print()
