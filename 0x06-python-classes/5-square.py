8e6c509b98a0fdbb2e0e7eac015b338bea9dc70a
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-10-24 16:14:24 -0700

    Task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Define a Square"""
+
+    def __init__(self, size=0):
+        """contructor.
+        Args:
+            size: size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """return new size of square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print in stdout the square."""
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#", end="\n" if j == self.size - 1and i != j else "")
+        print()
