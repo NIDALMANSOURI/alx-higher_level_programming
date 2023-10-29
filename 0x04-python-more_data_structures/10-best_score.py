544d6182c9d416a6b9c0801dae29703e1a86eef8
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-10-29 07:02:46 -0700

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    best_score = 0
+    best_key = ""
+    for k, v in a_dictionary.items():
+        if v > best_score:
+            best_score = v
+            best_key = k
+    return best_key
