3d2c00a3846237fc96297ea73d499f9b2b76ecdd
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-10-29 07:04:33 -0700

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     best_score = 0
-    best_key = ""
+    best_key = None
     for k, v in a_dictionary.items():
         if v > best_score:
             best_score = v
